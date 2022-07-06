94a09856b5c6aee9a0f3c7f899dbfa97561055be
ibrahimF01 <matrix012>
2022-07-06 21:39:28 +0300

    1. deneme

--- src/HavaSahasi.java
+++
@@ -1,9 +0,0 @@
-public class HavaSahasi {
-    public static void main(String[] args) {
-        System.out.println("Görev raporu hazırlandı");
-        System.out.println("İlk görev bağlantıları kur!");
-
-        System.out.println("Yeni deneme");
-        System.out.println("ahmet mecit");
-    }
-}
---
+++ src/ibrahim.java
@@ -0,0 +1,5 @@
+public class ibrahim {
+    public static void main(String[] args) {
+        System.out.println("ben ibrahim");
+    }
+}
