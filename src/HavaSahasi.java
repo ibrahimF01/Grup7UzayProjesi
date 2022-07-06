93f923bc18bb00bed887d80a0de920595cb66f61
ibrahimF01 <matrix012>
2022-07-06 20:57:16 +0300

    1. Görev tamamlandı

---
+++ src/HavaSahasi.java
@@ -0,0 +1,6 @@
+public class HavaSahasi {
+    public static void main(String[] args) {
+        System.out.println("Görev raporu hazırlandı");
+        System.out.println("İlk görev bağlantıları kur!");
+    }
+}
